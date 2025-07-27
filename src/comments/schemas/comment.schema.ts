36a1f3d062d2748747ef153eae8507786142cf4a
SimonSchulz <zenkov241091@gmail.com>
2025-07-27 17:04:20 +0200

    fix create comment problem

--- src/comments/schemas/comment.schema.ts
+++ src/comments/schemas/comment.schema.ts
@@ -1,12 +1,11 @@
 import { Schema, model } from "mongoose";
-import { CommentEntity } from "../dto/comment.entity";
 import { CommentDocument } from "../types/comment";
 
 const CommentSchema = new Schema<CommentDocument>({
   content: { type: String, required: true },
   commentatorInfo: {
     userId: { type: String, required: true },
-    login: { type: String, required: true },
+    userLogin: { type: String, required: true },
   },
   createdAt: { type: String, required: true },
   postId: { type: String, required: true },
